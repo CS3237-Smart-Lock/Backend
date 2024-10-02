339e812f9b7726ee9f78742903c8ad7dcfc199b5
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-10-02 14:05:28 +0800

    update gitignore

--- App/server/server.py
+++ App/server/server.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/health")
+def health():
+    return "<p>health check ok!</p>"
+
+
